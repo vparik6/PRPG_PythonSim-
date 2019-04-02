700f1996085f1da076966873f899874361910b7a
sstephans <ssteph7@uic.edu>
2019-04-02 16:04:28 -0500

    instructions.py skeleton
    has the instructions we need to use, but needs updating from project 2 and some instructions are c&p of each other. needs fixing and testing

--- python-simulator/instructions.py
+++ python-simulator/instructions.py
@@ -1,111 +1,9 @@
 #r-types
 
-#status: done and checked
-#required: yes
-#arithmetic
-def addu(instruction, registers):
-
-    operand1 = registers.r[instruction.rs] #value in rs
-    operand2 =  registers.r[instruction.rt] #value in rt
-
-    registers.r[instruction.rd] = operand1 + operand2
-    registers.PC += 4
-    return registers
-
-def adds(instruction, registers):
-
-    operand1 = registers.r[instruction.rs] #value in rs
-    operand2 =  registers.r[instruction.rt] #value in rt
-
-    registers.r[instruction.rd] = operand1 + operand2
-    registers.PC += 4
-    return registers
-
-#status: done and checked
-#required: yes
-#arithmetic
-def sub(instruction, registers):
-    operand1 = registers.r[instruction.rs] #value in rs
-    operand2 =  registers.r[instruction.rt] #value in rt
-
-    registers.r[instruction.rd] = operand1 - operand2
-    registers.PC += 4
-    return registers
-
-
-#status: done and checked
-#required: yes
-#arithmetic
-def slt(instruction, registers):
-    operand1 = registers.r[instruction.rs] #value in rs
-    operand2 =  registers.r[instruction.rt] #value in rt
-
-    if operand1 < operand2:
-        registers.r[instruction.rd] = 1 #set value to rd to 1
-    else:
-        registers.r[instruction.rd] = 0 #set value to rd to 0
-    registers.PC += 4
-    return registers
-
-#status: done and checked
-#required: yes
-#logic
-def and_r(instruction, registers):
-    operand1 = registers.r[instruction.rs] #value in rs
-    operand2 = registers.r[instruction.rt] #value in rt
-
-    registers.r[instruction.rd] = operand1 & operand2
-    registers.PC += 4
-    return registers
-
-
-#status: done and checked
-#required: yes
-#logic
-def sll(instruction, registers):
-    operand1 = registers.r[instruction.rt] #value in rt
-    operand2 = instruction.sh #shifting value
-
-    registers.r[instruction.rd] = operand1 << operand2
-    registers.PC += 4
-    return registers
-
-#status: done and checked
-#required: yes
-#logic
-def srl(instruction, registers):
-    operand1 = registers.r[instruction.rt] #value in rt
-    operand2 = instruction.sh #shifting value
-
-    registers.r[instruction.rd] = operand1 >> operand2
-    registers.PC += 4
-    return registers
-#status: TO DO
-#required: just for our project
-#logic
-def andi(instruction, registers):
-    operand1 = registers.r[instruction.rs]
-    operand2 = instruction.imm
-
-    registers.r[instruction.rt] = operand1 & operand2
-    registers.PC += 4
-    return registers
-
-#status: TO DO
-#just for our project
-#logic
-def xor(instruction, registers):
-    operand1 = registers.r[instruction.rs] #value in rs
-    operand2 = registers.r[instruction.rt] #value in rt
-    registers.r[instruction.rd] = operand1 ^ operand2
-    registers.PC += 4
-    return registers
-
 #i-types
-
 def lui(instruction, registers):
     registers.r[instruction.rt] = instruction.imm << 8
-    registers.PC += 4
+    registers.PC += 1
     return registers
 
 #status: test
@@ -114,15 +12,7 @@ def addi(instruction, registers):
     operand1 = registers.r[instruction.rs]
     operand2 = instruction.imm
     registers.r[instruction.rt] = operand1 + operand2
-    registers.PC += 4
-    return registers
-
-def or_r(instruction, registers):
-    operand1 = registers.r[instruction.rs]
-    operand2 = registers.r[instruction.rt]
-
-    registers.r[instruction.rd] = operand1 | operand2
-    registers.PC += 4
+    registers.PC += 1
     return registers
 
 
@@ -134,33 +24,56 @@ def ori(instruction, registers):
     operand2 = instruction.imm
 
     registers.r[instruction.rt] = operand1 | operand2
-    registers.PC += 4
+    registers.PC += 1
     return registers
 
-#status: test
-#required: yes
-#branch
-def beq(instruction, registers):
-    operand1 = registers.r[instruction.rs]
-    operand2 = registers.r[instruction.rt]
 
-    if (operand1 == operand2):
-        registers.PC += (4 + (instruction.imm << 2))
-    else:
-        registers.PC += 4
+def andi(instruction, registers):
+    operand1 = registers.r[instruction.rs]
+    operand2 = instruction.imm
+
+    registers.r[instruction.rt] = operand1 | operand2
+    registers.PC += 1
     return registers
 
-#status: test
-#required: yes
-#branch
-def bne(instruction, registers):
+def subi(instruction, registers):
     operand1 = registers.r[instruction.rs]
-    operand2 = registers.r[instruction.rt]
+    operand2 = instruction.imm
 
-    if (operand1 != operand2):
-        registers.PC += (4 + (instruction.imm << 2))
-    else:
-        registers.PC += 4
+    registers.r[instruction.rt] = operand1 | operand2
+    registers.PC += 1
+    return registers
+
+def add_r(instruction, registers):
+    operand1 = registers.r[instruction.rs]
+    operand2 = instruction.imm
+
+    registers.r[instruction.rt] = operand1 | operand2
+    registers.PC += 1
+    return registers
+
+def mult(instruction, registers):
+    operand1 = registers.r[instruction.rs]
+    operand2 = instruction.imm
+
+    registers.r[instruction.rt] = operand1 | operand2
+    registers.PC += 1
+    return registers
+
+def srl(instruction, registers):
+    operand1 = registers.r[instruction.rs]
+    operand2 = instruction.imm
+
+    registers.r[instruction.rt] = operand1 | operand2
+    registers.PC += 1
+    return registers
+
+def and(instruction, registers):
+    operand1 = registers.r[instruction.rs]
+    operand2 = instruction.imm
+
+    registers.r[instruction.rt] = operand1 | operand2
+    registers.PC += 1
     return registers
 
 #status: DONE
@@ -170,7 +83,7 @@ def lw(instruction, registers, memory):
     addr_index = registers.r[instruction.rs]
     offset = instruction.imm
     registers.r[instruction.rt] = memory.val[memory.addr.index(addr_index + offset)]
-    registers.PC += 4
+    registers.PC += 1
     return registers
 
 #status: DONE
@@ -180,13 +93,5 @@ def sw(instruction, registers, memory):
     addr_index = registers.r[instruction.rs]
     offset = instruction.imm
     memory.val[memory.addr.index(addr_index + offset)] = registers.r[instruction.rt]
-    registers.PC += 4
+    registers.PC += 1
     return memory
-
-
-#j-types
-#need for project
-#branch
-def j(instruction, registers):
-    registers.PC = (instruction.addr << 2)
-    return registers
